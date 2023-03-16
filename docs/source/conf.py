413dd2848c16b87b92aec56ba886e32a575676d2
roosterMAP <lead.acrysoft@gmail.com>
2023-03-16 11:15:42 -0700

    config update
    theme update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,20 @@ release = '2023.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +37,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+epub_show_urls = 'footnote'
